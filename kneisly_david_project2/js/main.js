1cd9d3cdb22ad2476fc906702d06016406e49614
Tyler Kneisly <tkneisly@fullsail.edu>
2012-05-06 22:35:49 -0400

    Added JS and CSS subfolders and comments to JS file.

---
+++ kneisly_david_project2/js/main.js
@@ -0,0 +1,18 @@
+// Week 2, Project 2
+// David Tyler Kneisly
+// VFW 1204
+// Book Tracker
+
+// Wait until the DOM is ready
+
+
+
+// getElementById Function
+
+
+
+// Variable defaults
+
+
+
+// Set link & Submit Click Events
--- kneisly_david_project2/main.js
+++
@@ -1,5 +0,0 @@
-// Week 2, Project 2
-// David Tyler Kneisly
-// VFW 1204
-// Run Tracker
-
